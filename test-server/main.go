81b2f0131a23b2f9093745e6a1e15e741c950f8e
Konstantin Pereiaslov <perk11@perk11.info>
2024-09-21 16:34:00 -0500

    Implement healthcheck and delay before starting in test server

--- test-server/main.go
+++ test-server/main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"log"
 	"net"
+	"net/http"
 	"os"
+	"time"
 )
+
+var listeningStarted bool = false
 
 func main() {
 	port := flag.String("p", "", "Port to listen on (required)")
+	healthCheckApiPort := flag.String("healthcheck-port", "", "Healthcheck API port to listen on. If not specified, healthcheck API is disabled")
+	durationToSleepBeforeListening := flag.Duration("sleep-before-listening", 0, "How much time to sleep before listening starts, such as \"300ms\", \"-1.5h\" or \"2h45m\". Valid time units are \"ns\", \"us\" (or \"µs\"), \"ms\", \"s\", \"m\", \"h\". ")
+	durationToSleepBeforeListeningForHealthCheck := flag.Duration("sleep-before-listening-for-healthCheck", 0, "How much time to sleep before listening for healthcheck starts, such as \"300ms\", \"-1.5h\" or \"2h45m\". Valid time units are \"ns\", \"us\" (or \"µs\"), \"ms\", \"s\", \"m\", \"h\". ")
 	flag.Parse()
 
 	if *port == "" {
@@ -16,7 +25,21 @@ func main() {
 		flag.Usage() // Print usage information
 		os.Exit(1)
 	}
+	if *healthCheckApiPort != "" {
+		go healthCheckListen(healthCheckApiPort, durationToSleepBeforeListeningForHealthCheck)
+	}
+	listenOnMainPort(port, durationToSleepBeforeListening)
 
+}
+
+type HealthcheckResponse struct {
+	Message string `json:"message"`
+	Status  int    `json:"status"`
+}
+
+func listenOnMainPort(port *string, sleepDuration *time.Duration) {
+	time.Sleep(*sleepDuration)
+	listeningStarted = true
 	listener, err := net.Listen("tcp", ":"+*port)
 	if err != nil {
 		fmt.Println("Error listening:", err.Error())
@@ -29,8 +52,6 @@ func main() {
 		}
 	}(listener)
 	fmt.Printf("Listening on port %s\n", *port)
-
-	// Handle incoming connections
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
@@ -48,5 +69,39 @@ func main() {
 		if connectionCloseErr != nil {
 			fmt.Println("Error closing connection: ", connectionCloseErr.Error())
 		}
+	}
+}
+
+func healthCheckHandler(responseWriter http.ResponseWriter, request *http.Request) {
+	var response HealthcheckResponse
+	if listeningStarted {
+		response = HealthcheckResponse{
+			Message: "ok",
+			Status:  200,
+		}
+	} else {
+		response = HealthcheckResponse{
+			Message: "server_starting",
+			Status:  503,
+		}
+	}
+
+	fmt.Println("Sending healthcheck status code:", response.Status)
+	responseWriter.Header().Set("Content-Type", "application/json")
+	responseWriter.WriteHeader(response.Status)
+
+	if err := json.NewEncoder(responseWriter).Encode(response); err != nil {
+		http.Error(responseWriter, "Failed to encode JSON response", http.StatusInternalServerError)
+	}
+}
+func healthCheckListen(port *string, sleepDuration *time.Duration) {
+	time.Sleep(*sleepDuration)
+	// Set up a route and associate it with a handler function
+	http.HandleFunc("/", healthCheckHandler)
+	fmt.Printf("Listening for healthcheck on port %s\n", *port)
+
+	// Start the HTTP server
+	if err := http.ListenAndServe(":"+*port, nil); err != nil {
+		log.Fatalf("Could not start healthcheck server: %s\n", err.Error())
 	}
 }

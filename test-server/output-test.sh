21d9d795a6071783821d76256d6b3cdd305383f4
Konstantin Pereiaslov <perk11@perk11.info>
2026-07-31 20:04:19 -0500

    fix: more POSIX compliance in output-test.sh

--- test-server/output-test.sh
+++ test-server/output-test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-[ "$1" == "-stderr" ] && exec 1>&2
+[ "$1" = "-stderr" ] && exec 1>&2
 
 echo "I am a test"
 printf "This ends with a return\r"
@@ -7,7 +7,7 @@ printf "Windows style\r\nNext after CRLF\n"
 printf "split write one "
 printf "plus two\n"
 printf "alpha\nbeta\ngamma\n"
-printf "Null byte \x00 inside\n"
+printf "Null byte \000 inside\n"
 sleep 0.1
 echo "Emoji 😀 test"
 sleep 0.1
